8b518dd4d0f95f35ae7fb742aa2f1cdeb46a30cf
jiangyuqing <jiangyuqing@hopemobi.com>
2026-08-14 09:21:30 +0800

    Add Clashoo LuCI package to custom build

--- diy-partX.sh
+++ diy-partX.sh
@@ -15,6 +15,14 @@ set_config() {
     printf 'CONFIG_%s=y\n' "$symbol" >>.config
 }
 
+set_config_value() {
+    local symbol="$1"
+    local value="$2"
+    sed -i -e "/^CONFIG_${symbol}=.*/d" \
+        -e "/^# CONFIG_${symbol} is not set$/d" .config
+    printf 'CONFIG_%s=%s\n' "$symbol" "$value" >>.config
+}
+
 clone_custom_package() {
     local repo="$1"
     local branch="$2"
@@ -26,6 +34,7 @@ if [ ! -d feeds/packages ]; then
     add_feed lucky 'https://github.com/gdy666/luci-app-lucky.git;main'
     add_feed easytier 'https://github.com/EasyTier/luci-app-easytier.git;main'
     add_feed quickfile 'https://github.com/sbwml/luci-app-quickfile.git;main'
+    add_feed clashoo 'https://github.com/kenzok8/openwrt-clashoo.git;main'
     mkdir -p package/custom
     clone_custom_package \
         'https://github.com/eamonxg/luci-theme-aurora.git' master \
@@ -45,6 +54,12 @@ for symbol in \
     PACKAGE_luci-app-easytier \
     PACKAGE_luci-i18n-easytier-zh-cn \
     PACKAGE_luci-app-quickfile \
-    PACKAGE_luci-i18n-quickfile-zh-cn; do
+    PACKAGE_luci-i18n-quickfile-zh-cn \
+    PACKAGE_clashoo \
+    PACKAGE_luci-app-clashoo \
+    PACKAGE_luci-i18n-clashoo-zh-cn; do
     set_config "$symbol"
 done
+
+# Keep the larger x86 rootfs size isolated from upstream .config changes.
+set_config_value TARGET_ROOTFS_PARTSIZE 2048
